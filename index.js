5cf15056f1cf2bf837e0d951d4dc47a4c5c082ba
Andrew Starr <astarr93@outlook.com>
2020-09-06 05:43:09 -0400

    Added more questions to the inquirer prompt. Also added when conditionals on certain questions for prompt logic

--- index.js
+++ index.js
@@ -6,28 +6,80 @@ const welcomeMessage = "Create high-quality ReadMe.md files for your code reposi
 let questions = [
     {
         type: "input",
-        name: "repoTitle",
-        message: "What is the title of your project: "
+        name: "projectTitle",
+        message: "What is the title of your project?: "
     },
     {
         type: "input",
-        name: "repoDescription",
-        message: "Please write a brief description of your project: "
+        name: "projectAbout",
+        message: "How would you describe your project?: "
     },
     {
         type: "input",
-        name: "repoInstall",
-        message: "How do you install your project: "
+        name: "projectInstall",
+        message: "How do you install your project?: "
     },
     {
         type: "input",
-        name: "repoUsage",
-        message: "How do you use your project: "
+        name: "projectUsage",
+        message: "How do you use your project?: "
+    },
+    {
+        type: "confirm",
+        name: "haveProjectMedia",
+        message: "Do you have any media (GIFs, JPGs, etc.) of your project to add?: ",
+        default: true
     },
     {
         type: "input",
-        name: "repoCredits",
-        message: "Please provide credit to any contributors as necessary: "
+        name: "projectMedia",
+        message: "Please select the folder (No subfolders please!) that contains your media: ",
+        when: (answers) => answers.haveProjectMedia === true
+    },
+    {
+        type: "confirm",
+        name: "haveProjectLicense",
+        message: "Do you have any licenses to add to your project?: ",
+        default: false
+    },
+    {
+        type: "list",
+        name: "projectLicense",
+        message: "Please select a license to add to your project: ",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "MIT"
+        ],
+        when: (answers) => answers.haveProjectLicense === true
+    },
+    {
+        type: "confirm",
+        name: "haveProjectCredits",
+        message: "Do you have any project contributors to credit?: ",
+        default: false
+    },
+    {
+        type: "input",
+        name: "projectCredits",
+        message: "Please provide any GitHub usernames that contributed to the project: ",
+        when: (answers) => answers.haveProjectCredits === true
+    },
+    {
+        type: "confirm",
+        name: "haveProjectTests",
+        message: "Do you have any testing instructions to prvoide about your project?: ",
+        default: false
+    },
+    {
+        type: "input",
+        name: "projectTests",
+        message: "Please describe any relevant testing instructions for your Project: "
+    },
+    {
+        type: "input",
+        name: "Contacts",
+        message: "Please provide the GitHub username(s) that will be the point-of-contact for any questions: ",
     }
 ];
 
