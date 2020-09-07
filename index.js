8c6e4cfa98fd6c3861f0c4d29a432005534f7421
Andrew Starr <astarr93@outlook.com>
2020-09-06 23:45:56 -0400

    Moved inquirer prompt questions to a new file called questionbank.js - The module is exported and imported into index

--- index.js
+++ index.js
@@ -1,88 +1,8 @@
 const inquirer = require('inquirer');
 const figlet = require('figlet');
 const fs = require('fs');
-const welcomeMessage = "Create high-quality ReadMe.md files for your code repositories using a JavaScript CLI consisting of node packages.\n\nPlease provide the following information to receive your file: \n";
-
-let questions = [
-    {
-        type: "input",
-        name: "projectTitle",
-        message: "What is the title of your project?: "
-    },
-    {
-        type: "input",
-        name: "projectAbout",
-        message: "How would you describe your project?: "
-    },
-    {
-        type: "input",
-        name: "projectInstall",
-        message: "How do you install your project?: "
-    },
-    {
-        type: "input",
-        name: "projectUsage",
-        message: "How do you use your project?: "
-    },
-    {
-        type: "confirm",
-        name: "haveProjectMedia",
-        message: "Do you have any media (GIFs, JPGs, etc.) of your project to add?: ",
-        default: true
-    },
-    {
-        type: "input",
-        name: "projectMedia",
-        message: "Please select the folder (No subfolders please!) that contains your media: ",
-        when: (answers) => answers.haveProjectMedia === true
-    },
-    {
-        type: "confirm",
-        name: "haveProjectLicense",
-        message: "Do you have any licenses to add to your project?: ",
-        default: false
-    },
-    {
-        type: "list",
-        name: "projectLicense",
-        message: "Please select a license to add to your project: ",
-        choices: [
-            "Apache License 2.0",
-            "GNU GPLv3",
-            "MIT"
-        ],
-        when: (answers) => answers.haveProjectLicense === true
-    },
-    {
-        type: "confirm",
-        name: "haveProjectCredits",
-        message: "Do you have any project contributors to credit?: ",
-        default: false
-    },
-    {
-        type: "input",
-        name: "projectCredits",
-        message: "Please provide any GitHub usernames that contributed to the project: ",
-        when: (answers) => answers.haveProjectCredits === true
-    },
-    {
-        type: "confirm",
-        name: "haveProjectTests",
-        message: "Do you have any testing instructions to prvoide about your project?: ",
-        default: false
-    },
-    {
-        type: "input",
-        name: "projectTests",
-        message: "Please describe any relevant testing instructions for your Project: "
-    },
-    {
-        type: "input",
-        name: "Contacts",
-        message: "Please provide the GitHub username(s) that will be the point-of-contact for any questions: ",
-    }
-];
-
+const questions = require('./questionbank');
+const welcomeMessage = "Create high-quality ReadMe.md files for your code repositories using a CLI consisting of node.js packages.\n\nPlease provide the following information to receive your file: \n";
 
 figlet('ReadMe Generator', function (err, data) {
     if (err) {
@@ -91,9 +11,9 @@ figlet('ReadMe Generator', function (err, data) {
     else {
         console.log(`${data}\n${welcomeMessage}`);
     }
-    inquirer.prompt(questions).then((answers) => createFile("README.md", answers));
+    inquirer.prompt(questions).then((answers) => createFile(answers));
 })
 
 function createFile(answers) {
-    console.log("PROGRAM END");
+    console.log(answers);
 }
---
+++ questionbank.js
@@ -0,0 +1,98 @@
+let questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?: "
+    },
+    {
+        type: "input",
+        name: "projectAbout",
+        message: "How would you describe your project?: "
+    },
+    {
+        type: "input",
+        name: "projectInstall",
+        message: "How do you install your project?: "
+    },
+    {
+        type: "input",
+        name: "projectUsage",
+        message: "How do you use your project?: "
+    },
+    // {
+    //     type: "confirm",
+    //     name: "haveProjectMedia",
+    //     message: "Do you have any media (GIFs, JPGs, etc.) of your project to add?: ",
+    //     default: false
+    // },
+    // {
+    //     type: "fuzzypath",
+    //     name: "projectMediaPath",
+    //     message: "Please select the folder that contains your project media: ",
+    //     itemType: "directory",
+    //     default: "C:/",
+    //     when: (answers) => answers.haveProjectMedia === true
+    // },
+    {
+        type: "confirm",
+        name: "haveProjectLicense",
+        message: "Do you want to add a license to your project?: ",
+        default: false
+    },
+    {
+        type: "list",
+        name: "projectLicense",
+        message: "Please select a license to add to your project: ",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "MIT",
+            "NO LICENSE"
+        ],
+        when: (answers) => answers.haveProjectLicense === true
+    },
+    {
+        type: "confirm",
+        name: "haveProjectCredits",
+        message: "Do you have any project contributors to credit?: ",
+        default: false
+    },
+    {
+        type: "input",
+        name: "projectCredits",
+        message: "Please provide any GitHub usernames that contributed to the project: ",
+        when: (answers) => answers.haveProjectCredits === true
+    },
+    {
+        type: "confirm",
+        name: "haveProjectTest",
+        message: "Do you have any testing instructions to prvoide about your project?: ",
+        default: false
+    },
+    {
+        type: "input",
+        name: "projectTest",
+        message: "Please describe any relevant testing instructions for your Project: ",
+        when: (answers) => answers.haveProjectTest === true
+    },
+    // {
+    //     type: "confirm",
+    //     name: "haveProjectTestMedia",
+    //     message: "Do you have any supporting testing media (GIFs, JPGs, etc.) of your project to add?: ",
+    //     default: false,
+    //     when: (answers) => answers.haveProjectTest === true
+    // },
+    // {
+    //     type: "input",
+    //     name: "projectTestMedia",
+    //     message: "Please select the folder that contains your project media: ",
+    //     when: (answers) => answers.haveProjectTestMedia === true
+    // },
+    {
+        type: "input",
+        name: "support",
+        message: "Please provide the GitHub username(s) responsible for answering any additional questions about your project: ",
+    }
+];
+
+module.exports = questions;
